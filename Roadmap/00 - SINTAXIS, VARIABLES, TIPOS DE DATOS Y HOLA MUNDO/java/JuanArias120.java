f3d0d0cb1bb6a5d8205693f6c294bcce32d9ea96
Juan Felipe Arias Orozco <arias.juan.8609@eam.edu.co>
2026-02-08 01:08:27 +0000

    Repaso Trabajo 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/JuanArias120.java
@@ -0,0 +1,60 @@
+//java.com/es/
+
+//Comentario de una Linea: todo lo que sigue en esa linea es ignorado por el compilador es para notas cortas
+
+/*Comentario De Varias Lineas: Sirven para explicaciones largas
+o inhabilitar bloques de codigo */
+
+
+/**Comentario de documentacion: Son para generar documentacion HTML externa */
+
+
+//Para declarar e inicializar:
+
+int contador = 0;
+
+// Tipos Comunes
+
+int entero = 16;
+
+double decimal = 0.10;
+
+char caracter  = '$';
+
+boolean verdaderofalso = true;
+
+String nombre = "Juan";
+
+
+
+//Comenzar con una letra, simbolo, _ y tambien recomienda ultilizar camelCase ej: miVariableFvorita
+
+// Se pueden declarar multiples variables del mismo tipo en una sola linea ej: int nota1 = 5, nota2 = 3
+
+//Para crear una constante en java: se ultiliza la palabra final combinada con static para asegurar el valor no cambie tras su inicializacion
+
+// Sintaxis Basica:
+
+static final String autor = "Juan"; 
+
+static final int MAX_USUARIOS = 100;
+
+//Imprimir en la terminal de texto
+
+System.out.println(nombre);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
